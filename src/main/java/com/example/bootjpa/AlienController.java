dd14ce09af4e6f3d0a811d5bb0c52dc6ee4d6dc4
NitheshKannaS <nitheshkanna23@gmail.com>
2023-02-22 19:01:53 +0530

    Query Methods added

--- src/main/java/com/example/bootjpa/AlienController.java
+++ src/main/java/com/example/bootjpa/AlienController.java
@@ -1,16 +1,33 @@
 package com.example.bootjpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AlienController {
+    @Autowired
+    AlienRepo repo;
     @RequestMapping("/")
     public String home(){
         return "home.jsp";
     }
-    @RequestMapping(/"addAlien")
+    @RequestMapping("/addAlien")
     public String addAlien(Alien alien){
+        repo.save(alien);
         return "home.jsp";
+    }
+    @RequestMapping("/getAlien")
+    public ModelAndView getAlien(@RequestParam int aid){
+        ModelAndView mv = new ModelAndView("showAlien.jsp");
+        Alien alien = repo.findById(aid).orElse(new Alien());
+        mv.addObject(alien);
+        System.out.println(repo.findByAidGreaterThan(2));
+        System.out.println(repo.findByTech("AI"));
+        System.out.println(repo.findByTechSorted("nithesh"));
+        return mv;
     }
 }
--- src/main/java/com/example/bootjpa/AlienRepo.java
+++ src/main/java/com/example/bootjpa/AlienRepo.java
@@ -1,5 +1,14 @@
 package com.example.bootjpa;
 
-public interface AlienRepo {
+import java.util.*;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AlienRepo extends CrudRepository<Alien,Integer> {
+        List<Alien> findByTech(String tech);
+        List<Alien> findByAidGreaterThan(int aid);
+        @Query("from Alien where aname=?1 order by tech")
+        List<Alien> findByTechSorted(String aname);
 
 }
